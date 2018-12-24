e43a6653ee27050b47d197399088f4a04abbfd52
Rahulio <rahul.ramesh888@gmail.com>
2018-12-23 20:22:37 -0500

    More changes to header

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,13 +1,19 @@
 import React from "react";
 
 import SiteLogo from "../site-logo/site-logo.component";
+import Menu from "../menu/menu.component";
 import "./header.styles.scss";
 
 const Header = () => {
   return (
-    <>
-      <SiteLogo />
-    </>
+    <div className="header-container">
+      <div className="" header-container__logo>
+        <SiteLogo />
+      </div>
+      <div className="" header-container__menu>
+        <Menu />
+      </div>
+    </div>
   );
 };
 
--- src/components/site-logo/site-logo.component.js
+++ src/components/site-logo/site-logo.component.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Logo from "../../assets/logo.svg";
-import Menu from "../menu/menu.component";
 
 import "./site-logo.styles.scss";
 
@@ -9,7 +8,6 @@ const SiteLogo = () => {
     <div className="siteLogo">
       <img className="siteLogo__image" src={Logo} alt="Site logo" />
       <div className="siteLogo__text">Rafikis doing things</div>
-      <Menu />
     </div>
   );
 };
