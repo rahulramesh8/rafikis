da72cc0196362b2e1f4c59eff636914585a9d391
Rahulio <rahul.ramesh888@gmail.com>
2019-01-14 19:27:05 -0500

    Fix

--- src/components/footer/footer.component.js
+++ src/components/footer/footer.component.js
@@ -1,23 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 const Footer = () => {
-  const [newCount, setNewCount] = useState(10);
-  useEffect(() => {
-    console.log("Running effect for newCount!");
-  });
-
-  return (
-    <>
-      New Count: {newCount}
-      <button onClick={() => setNewCount(0)}>Reset</button>
-      <button onClick={() => setNewCount(prevNewCount => prevNewCount + 1)}>
-        +
-      </button>
-      <button onClick={() => setNewCount(prevNewCount => prevNewCount - 1)}>
-        -
-      </button>
-    </>
-  );
+  return <>Hello I am footer</>;
 };
 
 export default Footer;
